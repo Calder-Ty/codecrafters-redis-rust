386a43a99c1329b7e472e2a50820a34016c7ca9b
Tyler Calder <calder-ty.protonmail.com>
2024-04-05 14:18:21 -0600

    feat: Handle multiple connections at once

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{Read, Write};
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread::spawn;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,19 +13,25 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => loop {
-                let mut input = [0; 512];
-                let read_count = _stream
-                    .read(input.as_mut_slice())
-                    .expect("Could Not Read from Stream");
-                if read_count == 0 {
-                    break;
-                }
-                let _ = _stream.write_all(b"+PONG\r\n");
-            },
+            Ok(mut _stream) => {
+                spawn(move || handle_client(_stream));
+            }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    loop {
+        let mut input = [0; 512];
+        let read_count = stream
+            .read(input.as_mut_slice())
+            .expect("Could Not Read from Stream");
+        if read_count == 0 {
+            break;
+        }
+        let _ = stream.write_all(b"+PONG\r\n");
     }
 }
