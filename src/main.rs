aba6e57b13155615d18ec5399732ad86db4f78fc
Tyler Calder <calder-ty.protonmail.com>
2024-04-05 13:42:41 -0600

    feat: Respond to multiple commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::io::Write;
+use std::io::{Read, Write};
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::vec;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,11 +14,41 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-            let _ = _stream.write_all(b"+PONG\r\n");
+                handle_client(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+enum RedisCommand {
+    Ping,
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut input = [0; 512];
+    let _ = stream.read(input.as_mut_slice());
+    for cmd in parse_client_input(&input) {
+        match cmd {
+            RedisCommand::Ping => {
+                let _ = stream.write_all(b"+PONG\n\r");
+            },
+        };
+    }
+}
+
+fn parse_client_input(input: &[u8]) -> impl Iterator<Item = RedisCommand> {
+    let mut outs = vec![];
+    let mut cmd_buff = vec![];
+    for byte in input {
+        if *byte == b'\n' {
+            let cmd = RedisCommand::Ping;
+            outs.push(cmd);
+            cmd_buff.clear();
+        };
+        cmd_buff.push(byte);
+    }
+    outs.into_iter()
 }
