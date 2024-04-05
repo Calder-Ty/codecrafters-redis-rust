e11e42e5e00265077345b8a7327b1eaf77f07386
Tyler Calder <calder-ty.protonmail.com>
2024-04-05 12:43:46 -0600

    feat: Respond to Ping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
 
@@ -11,8 +12,8 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+            let _ = _stream.write_all(b"+PONG\r\n");
             }
             Err(e) => {
                 println!("error: {}", e);
